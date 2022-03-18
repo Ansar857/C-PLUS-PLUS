f442045e1989bc0dbed08197faae60bee1dcbc48
Ansar857 <64150961+Ansar857@users.noreply.github.com>
2022-03-18 07:39:30 -0700

    Add files via upload

---
+++ IF ELSE STATEMENT.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using  namespace std;
+
+int main()
+{
+    float x,y;
+    cout<<"Enter value1 = ";
+    cin>>x;
+    cout<<"Enter value2 = ";
+    cin>>y;
+    if(x>y){
+        cout<<"\n"<<x;
+        cout<<" is greater then  " <<y;
+        
+         
+    }
+    
+    else {
+            cout<<"\n"<<y;
+            cout<<" is less then  " <<x;
+
+        }
+}
