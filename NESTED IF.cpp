d7a1a129bcd85849fec9aef61d0e15488781ec30
Ansar857 <64150961+Ansar857@users.noreply.github.com>
2022-03-18 07:00:28 -0700

    Add files via upload

---
+++ NESTED IF.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using  namespace std;
+
+int main()
+{
+    float x,y;
+    cout<<"Enter value1 = ";
+    cin>>x;
+    cout<<"Enter value2 = ";
+    cin>>y;
+    if(x>y){
+        cout<<"\n"<<x;
+        cout<<" is greater then  " <<y;
+        cout<<"\n"<<y;
+        cout<<" is less then  " <<x;
+        if(x<y){
+            cout<<x;
+            cout<<" is less then  " <<y;
+            cout<<"\n"<<y;
+            cout<<" is greater then  " <<x;
+        }
+    }
+    
+
+    
+}
