9577361c729a5e89306d7f66a2cded0cd92e62d3
Ansar857 <64150961+Ansar857@users.noreply.github.com>
2022-03-18 07:46:57 -0700

    Add files via upload

---
+++ ELSEIF STATEMENT.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using  namespace std;
+
+int main()
+{
+    float x,y;
+    cout<<"Enter value1 = ";
+    cin>>x;
+    cout<<"Enter value2 = ";
+    cin>>y;
+    if(x>y){
+        cout<<"\n"<<x;
+        cout<<" is greater then  " <<y;
+    }
+        
+    else if (x=y){
+        cout<<"x is equal  y " ;
+    }
+        
+         
+    
+    else {
+            cout<<"\n"<<y;
+            cout<<" is less then  " <<x;
+
+        }
+}
