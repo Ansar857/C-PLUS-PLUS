e515bd1e8a17de0b22d6b8be45e7b0c05cae4ce8
Ansar857 <64150961+Ansar857@users.noreply.github.com>
2022-03-21 20:44:31 +0500

    Add files via upload

---
+++ Question1.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string first_name,last_name,age,Highest_qualification,Institute_name;
+    cout<<"first_name = ";
+    cin>>first_name;
+    cout<<"last_name = ";
+    cin>>last_name;
+    cout<<"age = ";
+    cin>>age;
+    cout<<"Highest_qualification = ";
+    cin>>Highest_qualification;
+    cout<<"Institute_name = ";
+    cin>>Institute_name;
+    cout<<"\nMy name is "+ first_name +" "+  last_name + ".";
+    cout<<"I am "+ age + " years old.";
+    cout<<"My qualification is " + Highest_qualification + ".From " + Institute_name + " university. ";
+}
